ee9046295521584855145df90b767c2669f5f085
Jongwon Lee <jongwon@Jongwons-MacBook-Pro.local>
2023-03-29 20:01:39 +0900

    single output

--- k_mean_clustering.py
+++ k_mean_clustering.py
@@ -1,62 +1,14 @@
+import os
+import re
 from KoBERTScore import BERTScore
 from sklearn.cluster import KMeans
 from transformers import BertModel, AutoModel, AutoTokenizer
-
-def main():
-    # model_name = "monologg/koelectra-base-v2-discriminator"
-    model_name = "beomi/kcbert-base"
-    bert_score = BERTScore(model_name, best_layer=12)
-
-    references = [
-        "배고파",
-        "배고파",
-        "배고파",
-        "배고파",
-    ]
-    candidates = [
-        "배고파",
-        "아 배고프다",
-        "아 배아파",
-        "아 배고프다",
-    ]
-
-    scores = bert_score(references, candidates, retrain_idf=False, batch_size=128)
-    print(scores)
-#     scores = bert_score(references, candidates, retrain_idf=True, batch_size=128)
-#     print(scores)
-
-    model = AutoModel.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    inputs = tokenizer(["너는 이름이 뭐야", "나는 김말이야"], return_tensors="pt")
-    outputs = model(inputs["input_ids"], attention_mask=inputs["attention_mask"])
-    print(len(outputs))
-    pooler_output = outputs["pooler_output"]
-    print(pooler_output)
-    print(pooler_output.size())
+from sentence_transformers import SentenceTransformer
 
 
-def get_cluster_kmeans():
+def get_cluster_kmeans(corpus, num_clusters, model_name):
 
-    num_clusters = 2
-    model_name = "beomi/kcbert-base"
-    model = AutoModel.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    # Corpus with example sentences
-    corpus = [
-        "배고파",
-        "아 배고프다",
-        "아 배아파",
-        "아 배가 너무 아프다",
-              ]
-    inputs = tokenizer(corpus, padding=True, return_tensors="pt")
-    outputs = model(inputs["input_ids"], attention_mask=inputs["attention_mask"])
-    pooler_output = outputs["pooler_output"]
-    # corpus_embeddings = pooler_output
-
-    from sentence_transformers import SentenceTransformer
     embedder = SentenceTransformer(model_name)
-    corpus_tensor = embedder.encode(corpus, convert_to_numpy=False)
     corpus_embeddings = embedder.encode(corpus, convert_to_numpy=True)
 
     # clustering_model = KMeans(n_clusters=num_clusters, random_state=random_state)
@@ -69,12 +21,47 @@ def get_cluster_kmeans():
     for sentence_id, cluster_id in enumerate(cluster_assignment):
         clustered_sentences[cluster_id].append(corpus[sentence_id])
 
+    # sort
+    for i in range(num_clusters):
+        clustered_sentences[i] = sorted(clustered_sentences[i])
+    """
     for i, cluster in enumerate(clustered_sentences):
         print("Cluster ", i+1)
         print(cluster)
         print("")
+    """
+    return clustered_sentences
+
+
+def main():
+    # read file
+    num_tabs = 2
+    utterances = []
+    data_dir = "./data/"
+    input_file = "text_call.tsv"
+    with open(os.path.join(data_dir, input_file), "r", encoding="utf-8") as f:
+        for l_idx, line in enumerate(f):
+            line = line.strip()
+            line_split = line.split("\t")
+            assert len(line_split) == num_tabs, f"line #{l_idx}: {line} [TAB] ill-formatted"
+            utterance = line_split[-1].strip()
+            re_matched = re.match(r"SPK(\d+)* ", utterance)
+            assert re_matched, f"line #{l_idx}: {line} [SPK] ill-formatted"
+            utterance = utterance[re_matched.span()[-1]:].strip()
+            utterances.append(utterance)
+
+    model_name = "beomi/kcbert-base"
+    num_clusters = 4
+    num_clusters = min(num_clusters, len(utterances))
+    print(f"num_clusters: {num_clusters}")
+    clustered_sentences = get_cluster_kmeans(corpus=utterances, num_clusters=num_clusters, model_name=model_name)
+
+    for c_idx in range(num_clusters):
+        print(f"{c_idx} / {num_clusters}")
+        with open(os.path.join(data_dir, f"{c_idx}.txt"), "w", encoding="utf-8") as out_f:
+            for sent in clustered_sentences[c_idx]:
+                out_f.write(f"{sent}\n")
 
 
 if __name__ == "__main__":
-    # main()
-    get_cluster_kmeans()
+    main()
